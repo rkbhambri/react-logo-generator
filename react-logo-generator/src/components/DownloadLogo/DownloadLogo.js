b7242c18c20e5ce75e3553d4289524317b8573bb
ram <ramkrishan@kontikilabs.com>
2019-03-12 17:47:24 +0530

    Minor fix in ui

--- react-logo-generator/src/components/DownloadLogo/DownloadLogo.js
+++ react-logo-generator/src/components/DownloadLogo/DownloadLogo.js
@@ -5,11 +5,19 @@ const DownloadLogo = (props) => {
         <div className="download">
             <div className="row ml-1 pt-5">
                 <div className="downloadSvg" style={{ width: '47%' }}>
-                    <button id="downloadLink" className="btn btn-primary downloadLink w-100" onClick={() => props.downloadSVG()}>Download SVG</button>
+                    <button
+                        id="downloadLink"
+                        className="btn btn-primary downloadLink w-100"
+                        onClick={() => props.downloadSVG()}
+                        style={{ background: '#448AFA', borderColor: '#448AFA' }}>Download SVG</button>
 
                 </div>
                 <div className="downloadJpg" style={{ width: '49%', paddingLeft: '10px' }}>
-                    <button id="downloadLinkForImage" className="btn btn-primary downloadLink w-100" onClick={() => props.downloadImage()}>Download Image</button>
+                    <button
+                        id="downloadLinkForImage"
+                        className="btn btn-primary downloadLink w-100"
+                        onClick={() => props.downloadImage()}
+                        style={{ background: '#448AFA', borderColor: '#448AFA' }}>Download Image</button>
                 </div>
             </div>
         </div>
