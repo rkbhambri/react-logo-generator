dab0c8008fc244562027975e81ee0a8781944856
ram <ramkrishan@kontikilabs.com>
2019-03-16 10:08:12 +0530

    Updates

---
+++ precache-manifest.47d8de592ff44e8529498f49cbee52b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f26ca1bdf08854d5bc5",
+    "url": "/react-logo-generator/static/js/runtime~main.1336068c.js"
+  },
+  {
+    "revision": "03174eaa4f0936c372e5",
+    "url": "/react-logo-generator/static/js/main.97d9ade8.chunk.js"
+  },
+  {
+    "revision": "3c3bac9d1d4cf6d1c928",
+    "url": "/react-logo-generator/static/js/2.3ae94572.chunk.js"
+  },
+  {
+    "revision": "03174eaa4f0936c372e5",
+    "url": "/react-logo-generator/static/css/main.426f540e.chunk.css"
+  },
+  {
+    "revision": "3c3bac9d1d4cf6d1c928",
+    "url": "/react-logo-generator/static/css/2.80319c6a.chunk.css"
+  },
+  {
+    "revision": "4324dcbb5a2b633d49b907144af1fb92",
+    "url": "/react-logo-generator/index.html"
+  }
+];
